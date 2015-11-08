2abf0bb5bc6b1213c9481bcb4baddd5bfd6fed32
Darren Douglas Lee aka Dee <darrendouglas.lee@gmail.com>
2015-11-07 21:46:04 -0500

    Stub of model using Redis

---
+++ mb-digest-api/model/model.js
@@ -0,0 +1,33 @@
+module.exports = (function() {
+
+  var express   = require('express');
+  var redis     = require('redis');
+  var mb_config = require('config/mb_config.json');
+  
+  if (app.get('env') == 'development') {
+    // To output objects for debugging
+    // console.log("/process request: " + util.inspect(request, false, null));
+    var util = require('util');
+  }
+
+  // Redis
+  // =============================================================================
+  // All configurations related to the mb-logging Mongo database.
+  if (app.get('env') == 'production') {
+    var redisUri = mb_config.redis.production.uri;
+    var redisPort = mb_config.redis.production.port;
+  }
+  else {
+    var redisUri = mb_config.redis.development.uri;
+    var redisPort = mb_config.redis.development.port;
+  }
+  var client = redis.createClient(redisPort, redisUri);
+  client.on('connect', function() {
+    console.log('- Redis connected!');
+  });
+  
+ 
+
+  return client;
+  
+})();
