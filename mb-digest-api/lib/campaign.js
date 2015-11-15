f1e86e730f0039f54732f857752d7c422bef28c0
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-11-14 20:51:06 -0500

    Functional /campaign POST

--- mb-digest-api/lib/campaign.js
+++ mb-digest-api/lib/campaign.js
@@ -24,12 +24,29 @@ function Campaign(model) {
  *  The response object in a POST callback.
  */
 Campaign.prototype.post = function(req, res) {
-  this.request = req;
+  this.request = req.body;
   this.response = res;
+  var key = this.request.nid + '-' + this.request.language
   
-  client.set(this.request.ket, this.request.value, function(err, reply) {
-    console.log(reply);
+  // Check to see if the key already exists
+  client.exists(key, function(err, reply) {
+    if (reply === 1) {
+        console.log(key + ' already exists');
+        res.send(201, reply);
+    } else {
+
+      // Set the value for the key
+      client.set(key, this.request.value, function(err, reply) {
+        var results = key + ": " + reply;
+        console.log(results);
+        res.send(201, reply);
+      });
+    }
   });
+
+  // Expire the key value after one hour, 60 seconds x 60 minutes
+  var ttl = 60 * 60;
+  client.expire(key, ttl);
   
 };
 
@@ -42,6 +59,14 @@ Campaign.prototype.post = function(req, res) {
  *   The response object in the GET callback.
  */
 Campaign.prototype.get = function(req, res) {
+  this.request = req.body;
+  this.response = res;
+
+  client.get(this.request.key, function(err, reply) {
+    var results = key + ": " + reply;
+    console.log(results);
+    res.send(201, reply);
+  });
 
 };
 
