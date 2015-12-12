dea443f4b657a897734b40378062b2903c41500b
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-12-11 19:36:36 -0500

    Adjust test to use Mocha - sexy

--- mb-logging-api/test.js
+++ mb-logging-api/test.js
@@ -1,10 +1,17 @@
 var request = require('supertest');
 var app = require('./mb-logging-api-server');
 
-request(app)
-  .get('/')
-  .expect(200)
-  .end(function(error) {
-    if (error) throw error;
-    console.log('Done');
-  });
+describe('Requests to the root (/api) path', function() {
+
+  it('Returns a 200 status code', function(done) {
+
+    request(app)
+      .get('/api')
+      .expect(200)
+      .end(function(error) {
+        if (error) throw error;
+        done();
+      });
+    });
+
+});
