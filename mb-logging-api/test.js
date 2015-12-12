9a1f5d8a0e201b5765bbe17e06e9811bf8f96aa1
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-12-11 21:16:46 -0500

    Test for JSON response from all endpoints for GET and POST methods

--- mb-logging-api/test.js
+++ mb-logging-api/test.js
@@ -13,7 +13,7 @@ describe('Requests to the root (/api) path', function() {
   it('GET: Returns JSON format', function(done) {
 
     request(app)
-      .get('/api/v1')
+      .get('/api')
       .expect('content-type', 'application/json', done);
   });
 
@@ -39,40 +39,68 @@ describe('Requests to v1 root (/api/v1) path', function() {
 
 describe('Requests to v1 imports (/api/v1/imports) path', function() {
 
-  it('POST: Returns a 400 status code when required parameters type, exists, source, origin, processed_timestamp and email or phone or drupal_uid are not defined.', function(done) {
+  it('POST: Returns a 400 status code when required parameters "type", "exists", "source", "origin", "processed_timestamp" and "email" OR "phone" or "drupal_uid" are not defined.', function(done) {
 
     request(app)
       .post('/api/v1/imports')
       .expect(400, done);
   });
 
+  it('POST: Returns JSON format', function(done) {
+
+    request(app)
+      .post('/api/v1/imports')
+      .expect('content-type', 'application/json', done);
+  });
+
 });
 
 describe('Requests to v1 imports (/api/v1/imports/summaries) path', function() {
 
-  it('POST: Returns a 400 status code when required parameters type,source, target_CSV_file, signup_count and skipped are not defined.', function(done) {
+  it('POST: Returns a 400 status code when required parameters "type", "source", "target_CSV_file", "signup_count" and "skipped" are not defined.', function(done) {
 
     request(app)
       .post('/api/v1/imports/summaries')
       .expect(400, done);
   });
 
+  it('POST: Returns JSON format', function(done) {
+
+    request(app)
+      .post('/api/v1/imports/summaries')
+      .expect('content-type', 'application/json', done);
+  });
+
 });
 
 describe('Requests to v1 imports (/api/v1/user/activity) path', function() {
 
-  it('POST: Returns a 400 status code when required parameter vote is not defined.', function(done) {
+  it('POST: Returns a 400 status code when required parameter "vote" is not defined.', function(done) {
 
     request(app)
       .post('/api/v1/user/activity')
       .expect(400, done);
   });
 
-it('GET: Returns a 400 status code when required parameter "type" and "source" are not defined.', function(done) {
+  it('POST: Returns JSON format', function(done) {
+
+    request(app)
+      .post('/api/v1/user/activity')
+      .expect('content-type', 'application/json', done);
+  });
+
+  it('GET: Returns a 400 status code when required parameter "type" and "source" are not defined.', function(done) {
 
     request(app)
       .get('/api/v1/user/activity')
       .expect(400, done);
+  });
+
+  it('GET: Returns JSON format', function(done) {
+
+    request(app)
+      .get('/api/v1/user/activity')
+      .expect('content-type', 'application/json', done);
   });
 
 });
