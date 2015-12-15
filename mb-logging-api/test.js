d259525489d897eb5d0533d3e490a0ba5d31c894
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-12-14 19:31:13 -0500

    Test coverage of POST to /imports

--- mb-logging-api/test.js
+++ mb-logging-api/test.js
@@ -10,6 +10,7 @@
  */
 
 var request = require('supertest');
+var should = require('should');
 var app = require('./mb-logging-api-server');
 
 describe('Requests to the root (/api) path', function() {
@@ -64,49 +65,51 @@ describe('Requests to v1 imports (/api/v1/imports) path', function() {
       .expect('content-type', 'application/json', done);
   });
 
-  /**
-   * Future test: POST to /api/v1/imports with the following parameters
-   * source=teenlife&exists=1&origin=TeenLife-12-12-15.csv&processed_timestamp=1425767744&type=user_import
-   *
-   * and the following body:
-   * var importEvent = {
-   *   "logging_timestamp": "1410285144",
-   *   "phone": "2345678901",
-   *   "phone_status": "Still a phone number",
-   *   "email": "test4@test.com",
-   *   "email_status": "We got this one too.",
-   *    "email_acquired_timestamp": "1310285144",
-   *    "drupal_uid": "23456789",
-   *    "drupal_email": "test4@test.com"
-   * };
-   *
-   * request(app)
-   *   .post('/api/v1/imports')
-   *   .send(importEvent)
-   *
-   * NOTE: Consider using "should" library. Note Express 3 example:
-   *   .end(function(err, res) {
-   *     if (err) {
-   *       throw err;
-   *     }
-   *     // this is should.js syntax, very clear
-   *    res.should.have.status(400);
-   *      done();
-   *     });
-   */
+  // @todo
+  it('GET: Lookup of missing import log entry returns 404 response code.', function(done) {
+
+  });
 
   it('POST: Addition of import log entry returns 201 response code.', function(done) {
 
+    urlParams = '?source=niche&exists=1&origin=Niche-12-12-15.csv&processed_timestamp=1000000000&type=user_import';
     request(app)
-      .post('/api/v1/imports')
-      .expect(201, done);
+      .post('/api/v1/imports' + urlParams)
+      .send({
+        "logging_timestamp": "1410000000",
+        "phone": "1234567890",
+        "phone_status": "Still a phone number",
+        "email": "test@test.com",
+        "email_status": "We got this one too.",
+        "email_acquired_timestamp": "1400000000",
+        "drupal_uid": "12000000",
+        "drupal_email": "test@test.com"
+      })
+      .expect("content-type", /json/)
+      .expect(201)
+      .end(function(err, response) {
+        if (err) {
+          throw err;
+        }
+        response.status.should.equal(201)
+        response.body.should.equal("OK")
+        done();
+      });
   });
 
-  it('POST: Returns JSON format', function(done) {
+  // @todo
+  it('GET: Lookup of import log entry returns 200 response code. Returned data is formatted as expected.', function(done) {
 
-    request(app)
-      .post('/api/v1/imports')
-      .expect('content-type', 'application/json', done);
+  });
+
+  // @todo
+  it('DELETE: Test import log entry returns 200 response code.', function(done) {
+
+  });
+
+  // @todo
+  it('GET: Lookup of deleted import log entry returns 404 response code.', function(done) {
+
   });
 
 });
