7307b46608f4e25d11aed7b86b0c46af5f617fa5
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-12-18 09:40:15 -0500

    user-transactoinals module stub

---
+++ mb-logging-api/lib/user-transactional.js
@@ -0,0 +1,57 @@
+/**
+ * Interface to the User Transactional model.
+ */
+
+// Support output of objects
+var util = require('util');
+
+/**
+ * Constructor to the Log object.
+ *
+ * @param model
+ *   The model of the log document.
+ */
+function UserTransactional(model) {
+  this.docModel = model;
+}
+
+/**
+ * Converts a timestamp in seconds to a Date object.
+ *
+ * @param timestamp
+ *   Timestamp in seconds.
+ */
+var convertToDate = function(timestamp) {
+  return new Date(timestamp * 1000);
+};
+
+/**
+ * Create a log document from the supplied values for a user activity.
+ *
+ * @param req
+ *  The request object in a POST callback.
+ * @param res
+ *  The response object in a POST callback.
+ */
+UserTransactional.prototype.post = function(req, res) {
+  this.request = req;
+  this.response = res;
+  var addArgs = {};
+
+};
+
+/**
+ * Retrieve existing user activity log documents. Example request GET:
+ * /api/v1/user/activity?type=vote&&source=AGG&offset=300&interval=300
+ *
+ * @param req
+ *   The request object in the GET callback.
+ * @param res
+ *   The response object in the GET callback.
+ */
+UserTransactional.prototype.get = function(req, res) {
+
+
+};
+
+module.exports = UserTransactional;
