e9363a8f6fe01504e6f3f51b54eb1ecf0f58f118
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-12-26 12:26:11 -0500

    Addition of global.rootRequire to allow require() calls to make reference to the root of the app

--- mb-logging-api/mb-logging-api-server.js
+++ mb-logging-api/mb-logging-api-server.js
@@ -1,19 +1,43 @@
+/**
+ * mb-loggingapi-server.js
+ *
+ * An application to provide endpoints for http (API) access to the mb-logging
+ * database. Details of the endpoints can be found in the README.md file.
+ */
+
+/**
+ * Utility to provide reference to the root of the application. Used by files
+ * in subdirectories to reference other files within the application directory
+ * structure.
+ *
+ * Reference: https://gist.github.com/branneman/8048520#7-the-wrapper
+ */
+global.rootRequire = function(name) {
+  return require(__dirname + '/' + name);
+}
+
+/**
+ * Modules used by the application. Installed in node_modules via "npm install"
+ * as defined in package.json.
+ */
 var express           = require('express');
 var mongoose          = require('mongoose');
 var bodyParser        = require('body-parser');
 var FileStreamRotator = require('file-stream-rotator');
 var fs                = require('fs');
 var morgan            = require('morgan');
- 
-var routes = require('./routes/routes');
-var logDirectory = __dirname + '/logs';
 
-/**
- * Express Setup
- */
+// Define routes and related modules to call when requests are made to
+// the route.
+var routes = require('./routes/routes');
+
+// Express Setup
 var app = express();
 
+// LOGGING
+// =============================================================================
 // ensure log directory exists
+var logDirectory = __dirname + '/logs';
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 // create a rotating write stream
@@ -27,7 +51,7 @@ var accessLogStream = FileStreamRotator.getStream({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Toggle tools and logging based on environment setting
+// toggle tools and logging based on environment setting
 if (app.get('env') == 'development' || app.get('env') == 'test') {
   // To output objects for debugging
   // console.log("/ request: " + util.inspect(request, false, null));
@@ -46,5 +70,10 @@ else if (app.get('env') == 'production') {
 // All of routes will be prefixed with /api
 app.use('/api', routes);
 
+// APP AS MODULE
+// =============================================================================
 // Assign to module to allow testing vs binding to a port - via
+// $ npm tests
+// vs
+// $ bin/mb-logging-api-server.
 module.exports = app;
